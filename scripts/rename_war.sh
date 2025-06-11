c6f22a9507ba65547e999a90b7a6c3da23ae2934
Akash Chandran <67275861+AkashChand6n@users.noreply.github.com>
2025-06-11 14:53:33 +0530

    Update rename_war.sh

--- scripts/rename_war.sh
+++ scripts/rename_war.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 echo ">>> Renaming HelloWorld-0.0.1-SNAPSHOT.war to ROOT.war"
-mv /opt/tomcat/webapps/HelloWorld-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/ROOT.war
+mv /opt/tomcat/webapps/HelloWorld-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/hello.war
