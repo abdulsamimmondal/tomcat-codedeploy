a7f8ba56c59d1ca0565ac8fa5f22c94c10e83c97
akashchand6n <akashanoop123456@gmail.com>
2025-06-11 10:14:07 +0530

    rename

--- scripts/rename_war.sh
+++ scripts/rename_war.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-echo ">>> Renaming awspipelinedemo.war to ROOT.war"
-mv /opt/tomcat/webapps/awspipelinedemo.war /opt/tomcat/webapps/ROOT.war
+echo ">>> Renaming HelloWorld-0.0.1-SNAPSHOT.war to ROOT.war"
+mv /opt/tomcat/webapps/HelloWorld-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/ROOT.war
